ea9b57c7fe115ee38770d67b53b2c4feebe80c79
hpratt2 <hpratt2@uncc.edu>
2015-10-21 16:46:00 +0000

    completed section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,5 +24,21 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:title, :description, :rating, :release_date)
   end
   
-  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes! movie_params
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
 end
